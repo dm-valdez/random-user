adfddc17228e9f17248998398051614e35415174
David Valdez <dmvdev.22@gmail.com>
2022-04-01 23:39:25 +0800

    add basic style for modal

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import React, { useState, useEffect, Fragment } from 'react'
+import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 import ReactPaginate from 'react-paginate';
 
 import './App.css';
-import RandomUser from './components/RandomUser';
 import NavBar from './components/NavBar';
+import RandomUser from './components/RandomUser';
 
+// const itemsPerPage = JSON.parse(localStorage.getItem("filter"));
+
+// useEffect(() => {
+//   window.localStorage.setItem("filter", JSON.stringify(userPerPage));
+// }, [userPerPage]);
+
+// function items(userPerPage) {
+//   const selectedItems = userPerPage.target.value;
+//   setUserPerPage(selectedItems);
+// }
 
 function App() {
   const [randomUsers, setRandomUsers] = useState([]);
   const [loading, setLoading] = useState(false);
 
   const [pageNumber, setPageNumber] = useState(0)
-  const userPerPage = 15;
+  const [userPerPage, setUserPerPage] = useState(15)
   const pagesVisited = pageNumber * userPerPage;
+
 
   // get data from the API
   useEffect(() => {
     setLoading(true);
-    axios.get('https://randomuser.me/api/?page=1&results=41')
+    axios.get('https://randomuser.me/api/?results=40')
       .then(res => {
         console.log(res.data.results);
         setRandomUsers(res.data.results);
@@ -51,10 +62,12 @@ function App() {
         nextLabel={"Next"}
         pageCount={pageCount}
         onPageChange={changePage}
-        containerClassName={"paginationButton"}
-        previousLinkClassName={"previousButton"}
-        nextLinkClassName={"nextButton"}
-        disabledClassName={"paginationDisabled"}
+        containerClassName={"pagination justify-content-center"}
+        pageClassName={"page-item"}
+        pageLinkClassName={"page-link"}
+        previousLinkClassName={"page-link"}
+        nextLinkClassName={"page-link"}
+        disabledClassName={"page-item disabled"}
         activeClassName={"paginationActive"}
       />
     </div>
