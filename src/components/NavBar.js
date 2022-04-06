67a28aa5db383d813e3d2ba685f1d350890bdcff
David Valdez <dmvdev.22@gmail.com>
2022-04-06 16:35:27 +0800

    fix route home

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,7 +5,7 @@ const NavBar = () => {
     return (
         <Navbar variant="dark" className="nav">
             <Container>
-                <Navbar.Brand href="#home" className='nav-brand'>
+                <Navbar.Brand href="/" className='nav-brand'>
                     Random User API
                 </Navbar.Brand>
             </Container>
